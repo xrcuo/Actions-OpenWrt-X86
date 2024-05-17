da1a964a35f7fff58492d881d45516419e37af79
xrcuo <836447856@qq.com>
2024-05-18 07:57:00 +0800

    b

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.25/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/kenzok8/small-package package/small-package
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
